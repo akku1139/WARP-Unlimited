b2fab883f7ae8f88c1b2f52c567b683c406a98b8
-akku- <151517253+akku1139@users.noreply.github.com>
2024-02-19 19:33:21 +0900

    いい感じ

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,13 @@ const randomInt = (len: number) => Number(String(Math.floor(Math.random() * (10 
 const randomChar = (): string => "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".at(Math.floor(Math.random() * 52));
 const randomStr = (len: number) => new Array(len).fill(0).map(randomChar).join("");
 
-const upgrade = async (WARP_CLIENT_ID: string) => {
+export const upgrade = async (WARP_CLIENT_ID: string) => {
   const install_id = randomStr(22);
-  return fetch(`https://api.cloudflareclient.com/v0a${randomInt(3)}/reg`, {
+  return (await fetch(`https://api.cloudflareclient.com/v0a${randomInt(3)}/reg`, {
     method: "post",
     headers: {
       "Content-Type": "application/json",
-      "User-Agent": "okhttp/3.12.1",
+      "User-Agent": "nicefetch/3.1.4",
     },
     body: JSON.stringify({
       key: randomStr(43) + "=",
@@ -20,5 +20,5 @@ const upgrade = async (WARP_CLIENT_ID: string) => {
       type: "Android",
       locale: "es_ES",
     })
-  });
+  })).ok;
 };
